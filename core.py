c0869c5a3cfa075e4a50d4b678bd713fe35ae352
Kim Dohwan <57842896+MeatRoast@users.noreply.github.com>
2021-03-29 10:08:30 +0900

    game msg / 활동 메세지

---
+++ core.py
@@ -0,0 +1,13 @@
+from discord.ext import commands, tasks
+from info import config
+from itertools import cycle
+
+status = cycle(
+    [        
+        '[ ;help ] 감사합니다!',
+        'Copyright ©2021 WaterLand. All rights reserve"'
+    ]
+)
+
+
+bot = commands.Bot(command_prefix=config['command_prefix'], description=config['description'])
